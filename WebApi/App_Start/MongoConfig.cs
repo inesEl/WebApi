0b6796dbff35f0ceb493d24a171e8cb582e3c5df
Ines El Euch <ieuch@vistaprint.com>
2017-11-10 18:07:45 +0100

    First commit

---
+++ WebApi/App_Start/MongoConfig.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi.Models;
+
+namespace WebApi.App_Start
+{
+    public class MongoConfig
+    {
+        public static void Seed()
+        {
+            var patients = PatientDb.Open();
+
+            if (!patients.AsQueryable().Any(p => p.Name == "dodo"))
+            {
+                var data = new List<Patient>()
+                {
+                    new Patient
+                    {
+                        Name = "dodo",
+                        Ailments = new List<Ailment> (){ new Ailment { Name= "Crazyyyy" } },
+                        Medications = new List<Medication>()
+                    },
+                     new Patient
+                    {
+                        Name = "toto",
+                        Ailments = new List<Ailment> (){ new Ailment { Name= "Biiitch" } },
+                        Medications = new List<Medication>(){ new Medication { Name= "Hard core", Doses= 100} }
+                    }
+                };
+
+                patients.InsertMany(data);
+            }
+        }
+    }
+}
---
+++ WebApi/Controllers/PatientsController.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [EnableCors("*", "*", "Get")]
+    public class PatientsController : ApiController
+    {
+        IMongoCollection<Patient> _patients;
+
+        public PatientsController()
+        {
+            _patients = PatientDb.Open();
+        }
+
+        public IEnumerable<Patient> Get()
+        {
+            return _patients.Find(_=> true).ToEnumerable();
+        }
+
+        public IHttpActionResult Get(string id)
+        {
+            var patient = _patients.Find(_ => _.Id == id).ToList();
+            if(patient.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(patient);
+        }
+        [Route("api/patients/{id}/medications")]
+        public IHttpActionResult GetMedications(string id)
+        {
+            var patient = _patients.Find(_ => _.Id == id).ToList();
+            if (patient.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(patient[0].Medications);
+        }
+    }
+}
